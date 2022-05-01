6603b5f2ebab7ecf75b4cf9dca42df4ac158b939
NadineRosenzopf <nadine.rosenzopf02@htl-salzburg.ac.at>
2022-05-01 11:38:45 +0200

    R1: Eier hinzugefügt, Script angelegt

---
+++ Assets/MyGame/04Scripts/02Eier/Eier.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eier : MonoBehaviour
+{
+    public GameObject EiRotinCanvas;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "MainCamera")
+        {
+            Debug.Log("Osterei");
+            Destroy(gameObject);
+            EiRotinCanvas.SetActive(true);
+
+        }
+
+
+    }
+}
